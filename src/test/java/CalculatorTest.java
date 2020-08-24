c604ddd3c5a91762b45a3557c9477ebef4365be8
Helena <inger.helena@gmail.com>
2020-08-24 15:07:48 +0100

    canMultiplyNumbers pass

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,4 +20,9 @@ public class CalculatorTest {
     public void canSubtractNumbers() {
         assertEquals(5, calculator.subtractNumbers(10, 5));
     }
+
+    @Test
+    public void canMultiplyNumbers() {
+        assertEquals(20, calculator.multiplyNumbers(4, 5));
+    }
 }
