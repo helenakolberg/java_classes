401088d1f6855470c4a30414f5e7fa911931b2a2
Helena <inger.helena@gmail.com>
2020-08-24 15:44:22 +0100

    hasSheets pass

---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+    Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer(20);
+    }
+
+    @Test
+    public void hasSheets() {
+        assertEquals(20, printer.getSheets());
+    }
+
+}
